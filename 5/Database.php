3440a2ce174967ab383ae50cc2f27040f7940bf1
Elnur <57497680+ElnurMustafayev@users.noreply.github.com>
2020-09-18 10:46:16 +0400

    fix Database Task

--- 5/Database.php
+++ 5/Database.php
@@ -3,34 +3,31 @@
     class Database {
         
         /**
-         * Insert new value(s) in the $table
+         * Insert one new value in the $table
          *
          * @param  string   $table      table name to insert
-         * @param  array    $values     associative array [["k1" => "v1", "k2" => "v2"], ...]
-         * @return mixed                execute value or exception
+         * @param  array    $values     associative array ["k1" => "v1", "k2" => "v2", ...]
+         * @return mixed                return execute value or exit with exception
          */
-        function insert($table, $values = []) {
-            // (v1, v2), (v1, v2), (v1, v2), ...
-            // glue all parametr2 values with comma
-            $values_str =  implode(", ",
-                            // (value1, value2, ...)
-                            array_map(function($item) {
-                                    return "(" . implode(",", array_values($item)) . ")";
-                                }, $values));
+        function insert($table, $values) {
+            // if $values is empty
+            if(empty($values))
+                throw new Exception("There is nothing to insert");
 
-            // (key1, key2, key3, ...)
-            $keys_str = "(" . implode(",", array_keys($values[0])) . ")";
+            // if there is a connection to the server
+            if(!isset($this->conn)) 
+                exit("Error 404: Check your connection to the server");
+
+            // (key1, key2, ...)
+            $keys_str = "(" . implode(",", array_keys($values)) . ")";
+
+            // (v1, v2, ...)
+            $values_str =  "(" . implode(",", array_values($values)) . ")";
 
             // create sql query string
             $query = "INSERT INTO $table $keys_str VALUES $values_str;";
 
-            echo $query;
-
             try {
-                // if there is a connection to the server
-                if(!isset($this->conn))
-                    throw new Exception("Check your connection to the server", 404);
-
                 // try to prepare query
                 $insert = $this->conn->prepare($query);
 
@@ -49,9 +46,10 @@
 
     }
 
-    $values =   [["name" => "Elnur", "phone" => 21424124],
-                ["name" => "Ali", "phone" => 2362352]];
+    $values =   ["name" => "Elnur", "phone" => 21424124, "address" => "Baku"];
     
     (new Database())->insert("Users", $values);
+
+    // INSERT INTO Users (name,phone,address) VALUES (Elnur,21424124,Baku);
 
 ?>
